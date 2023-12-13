aa5da6da3cab7eaa3475a9f80437bf9e2cca050e
saleh <m.saleh0444@gmail.com>
2023-12-13 16:17:15 +0330

    beta

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess)
+    document.querySelector('.message').textContent = 'No Number (YFI)';
+  else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'You Won (YFAL)';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost The Game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost The Game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
